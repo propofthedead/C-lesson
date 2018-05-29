053f8b27301cc05d314755ba3d64d9a60b0d3123
Foster Stule <camodude5@yahoo.com>
2018-05-29 15:28:52 -0400

    intial commit and tested basic logics

--- CSharpLesson/Program.cs
+++ CSharpLesson/Program.cs
@@ -10,6 +10,24 @@ namespace CSharpLesson
 	{
 		static void Main(string[] args)
 		{
+			int a=23*37;
+			int b = a -41;
+			string aStr = a.ToString();
+			Console.WriteLine(b/7);
+			string name = "Foster";
+			int days = 6;
+			Console.WriteLine("My name is " + name + " I've been in bootcamp for " + days + " days");
+			int byear = 1997;
+			int bmonth = 4;
+			int bday = 8;
+			int year = 2018;
+			int month = 5;
+			int day = 29;
+			int years = year - byear;
+			int dayss = (30 - bday) + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31;
+			int dayyears = 31 + 28 + 31 + 30 + 29;
+			int daysold = (years * 365) + dayss + dayyears;
+			Console.WriteLine(daysold);
 		}
 	}
 }
---
+++ CSharpLesson/alive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLesson
+{
+	class alive
+	{
+		static void main(string[] args)
+		{
+			int byear = 1997;
+			int bmonth = 4;
+			int bday = 8;
+			int year = 2018;
+			int month = 5;
+			int day = 29;
+			int years = year - byear;
+			int days = (30 - bday) + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31;
+			int dayyears = 31 + 28 + 31 + 30 + 29;
+			int daysold = (years * 365) + day + dayyears;
+			Console.WriteLine(daysold);
+		}
+	}
+}
